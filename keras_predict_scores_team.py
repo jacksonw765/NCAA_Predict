c2687bd52eae88d3fbe3ea70c26a43e8dd0512e5
Jackson Welch <jacksonwelch@Jacksons-MacBook-Pro.local>
2021-12-05 00:03:50 -0500

    split game into two tests

--- keras_predict_scores_team.py
+++ keras_predict_scores_team.py
@@ -2,13 +2,18 @@ import pandas as pd
 #import xgboost as xgb
 #from sklearn import preprocessing
 #from sklearn.impute import SimpleImputer
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.impute import SimpleImputer
 from sklearn.linear_model import *
 #from sklearn.metrics import r2_score, mean_squared_error
+from sklearn.metrics import r2_score
 from sklearn.model_selection import train_test_split
 import numpy as np
 
 #from sklearn.pipeline import Pipeline
 #from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler
 
 columns_drop = ['away_losses', 'away_wins', 'conference_losses', 'conference_wins', 'games_played', 'home_losses',
                 'home_wins', 'losses', 'wins',  'away_losses_2', 'away_wins_2', 'conference_losses_2', 'conference_wins_2',
@@ -41,33 +46,35 @@ def get_game(home_team, away_team, location=2):
     return append_team
 
 
-master_df = pd.read_csv('team_list_scores2.csv')
-y = master_df[['points_for', 'points_against']].to_numpy().tolist()
+master_df = pd.read_csv('team_list_scores2021.csv')
+y = master_df['points_for'].to_numpy().tolist()
 master_df = master_df.drop(columns=['abbreviation', 'abbreviation_2', 'Unnamed: 0.1','Unnamed: 0',  'Unnamed: 0_2', 'points_for', 'points_against'])
 master_df = master_df.drop(columns=columns_drop)
 X = np.round(master_df.to_numpy(), 2).tolist()
-preds = []
-num_sim = 100
-away_team = "SOUTHERN-CALIFORNIA"
-home_team = "WASHINGTON-STATE"
-predict_X = get_game(home_team, away_team)
-for x in range(0, num_sim):
-    X_train, X_test, y_train, y_test = train_test_split(X, y)
-    model = LinearRegression()
-    # model = Pipeline([
-    #     ('impute', SimpleImputer(strategy='mean')),
-    #     ('scale', MinMaxScaler())
-    # ])
-    results = model.fit(X_train, y_train)
-    #print(model.score(X_train, y_train))
 
-    predictions = model.predict(predict_X.to_numpy())
-    pred = np.round(predictions, 0).tolist()
-    #test_set_r2 = r2_score(y_test, predictions)
-    preds.append(pred[0])
+num_sim = 1
+team1 = "ALABAMA"
+team2 = "GONZAGA"
 
-df = pd.DataFrame(preds, columns=[home_team, away_team])
-print(home_team + ": " + str(round(df[home_team].mean(), 0)))
-print(away_team + ": " + str(round(df[away_team].mean(),0)))
-print(str(round((float(len(preds)/num_sim)), 2)))
+predict_X_1 = get_game(team1, team2, location=0)
+predict_X_2 = get_game(team2, team1, location=2)
+
+def simulate_game(team, predict_data):
+    preds = []
+    for x in range(0, num_sim):
+        X_train, X_test, y_train, y_test = train_test_split(X, y)
+        #model = LogisticRegression(max_iter=10000, penalty="l2", fit_intercept=False, multi_class="ovr", C=1, solver = 'lbfgs')
+        #model = RandomForestRegressor(n_estimators=200)
+        model = LinearRegression()
+        model.fit(X_train, y_train)
+        print(model.score(X_train, y_train))
+        predictions = model.predict(predict_data.to_numpy())
+        pred = np.round(predictions, 0).tolist()
+        preds.append(pred)
+    df = pd.DataFrame(preds, columns=[team])
+    return int(round(df[team].mean(), 0))
+
+
+print(team1 + ": " + str(simulate_game(team1, predict_X_1)))
+print(team2 + ": " + str(simulate_game(team2, predict_X_2)))
 
